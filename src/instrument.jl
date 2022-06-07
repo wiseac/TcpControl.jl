52472d0c4a1f4c662ee44ece7042af9f2cd5a6f6
wiseac <89418908+wiseac@users.noreply.github.com>
2022-06-06 21:59:55 -0400

    Update instrument.jl

--- src/instrument.jl
+++ src/instrument.jl
@@ -55,9 +55,9 @@ function CreateTcpInstr(model, address)
     Instr{model}(model, address, TCPSocket(), false)
 end
 
-function Base.show(io::IO, ::MIME"text/plain", i::TcpControl.Instr)
+function Base.show(io::IO, ::MIME"text/plain", i::TcpDevices.Instr)
     model = i.model isa DataType ? i.model : typeof(i.model)
-    println("TcpControl.Instr{$(i.model)}")
+    println("TcpDevices.Instr{$(i.model)}")
     println("    Group: $(supertype(model))")
     println("    Model: $(model)")
     println("    Address: $(i.address)")
