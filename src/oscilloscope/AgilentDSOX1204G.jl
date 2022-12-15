e6db6bc606ee879a007b1355faabb5aa7ec2c7f3
Morten F. Rasmussen <10264458+mofii@users.noreply.github.com>
2022-12-15 10:25:19 -0800

    Remove unnecessary `where T` (#194)

--- src/oscilloscope/AgilentDSOX1204G.jl
+++ src/oscilloscope/AgilentDSOX1204G.jl
@@ -1,4 +1,4 @@
-function set_impedance_50ohm(instr::Instrument{T}; chan=1) where {T <: AgilentDSOX1204G}
-    error(string(T) * " impedance is fixed to 1 MΩ")
+function set_impedance_50ohm(instr::Instrument{AgilentDSOX1204G}; chan=1)
+    error("AgilentDSOX1204G impedance is fixed to 1 MΩ")
     return nothing
 end
